8d52179b79823a1ef199cfc35d5995a355516d2b
Dominik Gresch <greschd@gmx.ch>
2017-05-12 11:32:33 +0200

    Start defining plot function

--- bandstructure_utils/plot.py
+++ bandstructure_utils/plot.py
@@ -2,3 +2,12 @@
 # -*- coding: utf-8 -*-
 #
 # Author:  Dominik Gresch <greschd@gmx.ch>
+
+import numpy as np
+import scipy.linalg as la
+import matplotlib.pyplot as plt
+
+from .kpoints import KpointsPath
+
+def plot(eigenvals, *, special_labels={}):
+    kpoints = eigenvals.kpoints
