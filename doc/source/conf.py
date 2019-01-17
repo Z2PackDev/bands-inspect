0a1c3545bca330109750946f342be6e09a68290d
Dominik Gresch <greschd@gmx.ch>
2019-01-17 22:26:42 +0100

    Disable mocking on RTD.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -9,20 +9,20 @@ import time
 
 import sphinx_rtd_theme
 
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
-if on_rtd:
-    from unittest.mock import MagicMock
-
-    class Mock(MagicMock):
-        @classmethod
-        def __getattr__(cls, name):
-            return MagicMock()
-
-    MOCK_MODULES = [
-        'h5py', 'numpy', 'matplotlib', 'matplotlib.pyplot', 'scipy',
-        'scipy.linalg'
-    ]
-    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+# on_rtd = os.environ.get('READTHEDOCS') == 'True'
+# if on_rtd:
+#     from unittest.mock import MagicMock
+#
+#     class Mock(MagicMock):
+#         @classmethod
+#         def __getattr__(cls, name):
+#             return MagicMock()
+#
+#     MOCK_MODULES = [
+#         'h5py', 'matplotlib', 'matplotlib.pyplot', 'scipy',
+#         'scipy.linalg'
+#     ]
+#     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
